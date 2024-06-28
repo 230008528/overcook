608d13b4ca16b99114c57c6d7df48831387e3c87
230008528 <230008528@stu.vtc.edu.hk>
2024-06-29 05:00:23 +0800

    Scene Transition Added

--- Assets/AchievementSystem/introLogo/AutoChangToMainMenu.cs
+++ Assets/AchievementSystem/introLogo/AutoChangToMainMenu.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class AutoChangToMainMenu : MonoBehaviour
 {
-   
+    public static AutoChangToMainMenu sceneController;
+    [SerializeField] Animator transitionAnim;
+    private void Awake() { }
+
     public void StartMainMenu()
     {
-        SceneManager.LoadSceneAsync("MainMenuScene");
+        StartCoroutine(LoadMainMenu());
+        // SceneManager.LoadSceneAsync("MainMenuScene");
     }
 
-   
+   IEnumerator LoadMainMenu()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync("MainMenuScene");
+        //transitionAnim.SetTrigger("Start");
+    }
+
+
 }
