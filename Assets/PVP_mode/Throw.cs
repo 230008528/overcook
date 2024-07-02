b8eba855872eb1fefd10008845efa781e8aa0925
230008528 <230008528@stu.vtc.edu.hk>
2024-07-02 17:18:33 +0800

    added

---
+++ Assets/PVP_mode/Throw.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+   public LineRenderer line;
+    public Transform throwedObject;
+    public int length=1000;
+    public float time;
+    public float speed;
+    public  GameObject objToThorow;
+    
+    public GameObject player1;
+
+    public void Start()
+    {
+        //player1= GameObject.FindGameObjec;
+    }
+    private void Update()
+    {
+        //if(Input.GetButtonDown(""))
+    }
+
+}
